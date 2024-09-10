00e05f7c86573e85f9a68b1a2b34349ef975e298
MercyHeart <mikukon1@outlook.com>
2024-09-10 18:53:21 +0200

    modify the script

--- 0.build.sh
+++ 0.build.sh
@@ -1,8 +1,9 @@
-# git pull
+git pull
 make -j48
 make install
 cd ../../
 source 0.build_src.sh
-mkfs.erofs -zlzma  arm64_lzma_4k.img ./aarch64_elf_bin/
-mkfs.erofs -zlzma  --bcj=arm64 arm64_bcj_lzma_4k.img ./aarch64_elf_bin/
+# mkfs.erofs -zlzma  arm64_lzma_4k.img ./aarch64_elf_bin/
+# mkfs.erofs -zlzma  --bcj=arm64 arm64_bcj_lzma_4k.img ./aarch64_elf_bin/
+mkfs.erofs -zlzma  arm64_lzma_4k_test.img ./arm64test/
 cd 0.src/erofs-utils-x
