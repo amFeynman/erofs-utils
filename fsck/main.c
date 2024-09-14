a60fa78d3e22a7fcc558807a4a9219d5ae704167
MercyHeart <mikukon1@outlook.com>
2024-09-15 00:22:20 +0200

    double read block decompress test7

--- fsck/main.c
+++ fsck/main.c
@@ -521,12 +521,11 @@ static int erofs_verify_inode_data(struct erofs_inode *inode, int outfd)
 			}
 			ret = z_erofs_read_one_data(inode, &map, raw, buffer,
 						    0, map.m_llen, false);
-			erofs_err("decompressed %d,bcjflag = %d",map.m_llen,cfg.c_bcj_flag);
-			if(cfg.c_bcj_flag){
+			if(g_sbi.bcj_flag){
 				bcj_code((uint8_t *)buffer,0,(size_t)map.m_llen,cfg.c_bcj_flag,false);
-				erofs_err("decompressed %d",map.m_llen);
+				erofs_err("bcj decode %d",map.m_llen);
 			}
-			if(cfg.c_bcj_flag && map.m_plen == map.m_llen){
+			if(g_sbi.bcj_flag && map.m_plen == map.m_llen){
 				erofs_err("nocompress %d",map.m_llen);
 			}
 
