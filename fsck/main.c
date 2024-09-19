758f80a11b4a45ffbdf281ca2338373b56df21c1
MercyHeart <mikukon1@outlook.com>
2024-09-19 15:26:16 +0200

    double read block decompress test26

--- fsck/main.c
+++ fsck/main.c
@@ -526,7 +526,7 @@ static int erofs_verify_inode_data(struct erofs_inode *inode, int outfd)
 			}else if(g_sbi.bcj_flag && map.m_plen == map.m_llen){
 				erofs_err("nocompress %d",map.m_llen);
 				char* temp = buffer;
-				erofs_err(*temp);
+				erofs_err("%d",*temp);
 			}
 
 			if (ret)
