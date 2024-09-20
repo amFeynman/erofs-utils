e02c10025e4d9ca5f024d5e954c10a19a2cb9d75
MercyHeart <mikukon1@outlook.com>
2024-09-20 22:16:40 +0200

    map.m_la as now position

--- fsck/main.c
+++ fsck/main.c
@@ -521,7 +521,7 @@ static int erofs_verify_inode_data(struct erofs_inode *inode, int outfd)
 			ret = z_erofs_read_one_data(inode, &map, raw, buffer,
 						    0, map.m_llen, false);
 			if(g_sbi.bcj_flag && map.m_plen != map.m_llen){
-				bcj_code((uint8_t *)buffer,0,(size_t)map.m_llen,g_sbi.bcj_flag,false);
+				bcj_code((uint8_t *)buffer,(uint32_t)map.m_la,(size_t)map.m_llen,g_sbi.bcj_flag,false);
 				erofs_err("bcj decode %d,pos = %d,map.m_la = %d",map.m_llen,pos,map.m_la);
 			}else if(g_sbi.bcj_flag && map.m_plen == map.m_llen){
 				erofs_err("nocompress %d",map.m_llen);
