4b53039cbc776cc6a07451ab7b91ee7f52a9f1f7
MercyHeart <mikukon1@outlook.com>
2024-09-22 01:45:03 +0200

    debug test7

--- 0.build.sh
+++ 0.build.sh
@@ -6,7 +6,7 @@ source 0.build_src.sh
 # mkfs.erofs -zlzma  arm64_lzma_4k.img ./aarch64_elf_bin/
 # mkfs.erofs -zlzma  --bcj=arm64 arm64_bcj_lzma_4k.img ./aarch64_elf_bin/
 mkfs.erofs -zlzma --bcj=arm64 arm64_bcj_lzma_4k_test.img ./arm64test/
-fsck.erofs --extract=out arm64_bcj_lzma_4k.img
+fsck.erofs --extract=out arm64_bcj_lzma_4k_test.img
 md5sum arm64test/*
 md5sum out/*
 rm -rf out
