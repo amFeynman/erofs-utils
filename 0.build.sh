a5b3896a1b151b30c84ad93c61a49d7ebdc34cc6
MercyHeart <mikukon1@outlook.com>
2024-09-22 02:27:48 +0200

    debug test11

--- 0.build.sh
+++ 0.build.sh
@@ -3,11 +3,10 @@ make -j48
 make install
 cd ../../
 source 0.build_src.sh
-# mkfs.erofs -zlzma  arm64_lzma_4k.img ./aarch64_elf_bin/
-# mkfs.erofs -zlzma  --bcj=arm64 arm64_bcj_lzma_4k.img ./aarch64_elf_bin/
-mkfs.erofs -zlzma --bcj=arm64 arm64_bcj_lzma_4k_test.img ./arm64test/
-fsck.erofs --extract=out arm64_bcj_lzma_4k_test.img
-md5sum arm64test/*
+mkfs.erofs -zlzma  --bcj=arm64 arm64_bcj_lzma_4k.img ./aarch64_elf_bin/
+# mkfs.erofs -zlzma --bcj=arm64 arm64_bcj_lzma_4k_test.img ./arm64test/
+fsck.erofs --extract=out arm64_bcj_lzma_4k.img
+md5sum aarch64_elf_bin/*
 md5sum out/*
 rm -rf out
 
